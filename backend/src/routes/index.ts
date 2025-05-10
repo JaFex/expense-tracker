a2219a9a47cba7fd278944722a7c2bb07834cc4d
jafelix <jafelix.22@gmail.com>
2025-05-10 15:52:23 +0100

    chore(backend): remove swagger
    This will need more research to find a good aproach'

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,25 +1,7 @@
 import { Router } from 'express';
 
-/**
- * @swagger
- *
- * tags:
- *  - name: Main
- */
-
 export const mainRouter = Router();
 
-/**
- *  @openapi
- * /:
- *  get:
- *      tags:
- *      - Main
- *      description: Hello world
- *      responses:
- *          200:
- *              description: Returns Hello Wold! string
- */
 mainRouter.get('/', (req, res) => {
 	res.send('Hello World!');
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,9 @@
-import swaggerUi from 'swagger-ui-express';
 import { app } from './app';
 import { configs } from './configs/configs';
 import { swaggerDocs } from './configs/swagger';
 import { logger } from './tools/logger';
 
 const port = configs.PORT;
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
 	logger.info(`Listening on port ${port}`);
